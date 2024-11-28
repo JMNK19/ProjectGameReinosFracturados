aad83f23b2c119c6b714e2dda4a1ac723f50855e
Jean <95231763+JMNK19@users.noreply.github.com>
2024-11-27 23:37:30 -0500

    Fondo del laboratorio, labels y asistente

---
+++ scenes/characters/asistente/asistente.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 650
+
+func _ready():
+	animated_sprite_2d.play("idle")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	move_and_slide()
