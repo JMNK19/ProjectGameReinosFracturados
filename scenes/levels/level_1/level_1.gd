61cf94b630e03d3eea00f6373364fb81a8296998
Jean <95231763+JMNK19@users.noreply.github.com>
2024-12-01 01:24:46 -0500

    labotario, corregi un error del main_character en el dialogo  y  agregue movimiento al NPC Asistente y mas dialogo y efectos
    el main_character se podia mover cuando estaba en dialogo (arreglado), el asistente se mueve hacia la maquina y cuando llega el jugador se activa el dialogo el portal y efecto de iluminación.

--- scenes/levels/level_1/level_1.gd
+++ scenes/levels/level_1/level_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var animation_player_control = $AnimationPlayerControl
 @onready var animation_player_control_2 = $AnimationPlayerControl2
+@onready var animation_player_portal_activado = $AnimationPlayerPortalActivado
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,3 +20,8 @@ func _process(delta):
 func _on_animation_player_control_animation_finished(anim_name):
 	animation_player_control_2.play("Controles")
 	pass # Replace with function body.
+
+
+func _on_area_2d_portal_activado_body_entered(body):
+	if body.is_in_group("player"):
+		animation_player_portal_activado.play("portal_activado") # Replace with function body.
